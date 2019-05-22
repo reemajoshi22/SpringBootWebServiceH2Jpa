df83f738ec80bc925c381640059621bb57ccafa0
reema.joshi <reema.joshi@newgen.co.in>
2019-05-22 15:57:30 +0530

    alternate way of defining mapping.

--- src/main/java/com/learning/springboot/domain/EmployeeController.java
+++ src/main/java/com/learning/springboot/domain/EmployeeController.java
@@ -10,22 +10,26 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @GetMapping("/employee")
+    //@GetMapping("/employee")
+    @RequestMapping(value = "/employee",method = RequestMethod.GET)
     private List<Employee> getAllEmployee() {
         return employeeService.getAllEmployee();
     }
 
-    @GetMapping("/employee/{id}")
+    //@GetMapping("/employee/{id}")
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.GET)
     private Employee getPerson(@PathVariable("id") int id) {
         return employeeService.getEmployeeById(id);
     }
 
-    @DeleteMapping("/employee/{id}")
+    //@DeleteMapping("/employee/{id}")
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
     private void deletePerson(@PathVariable("id") int id) {
         employeeService.delete(id);
     }
 
-    @PostMapping("/employee")
+    //@PostMapping("/employee")
+    @RequestMapping(value = "/employee",method = RequestMethod.POST)
     private int savePerson(@RequestBody Employee employee) {
         employeeService.saveOrUpdate(employee);
         return employee.getEmp_id();
