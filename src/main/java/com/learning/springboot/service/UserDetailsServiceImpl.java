8cf6aef2f6726db631d9ea39551167f6c22140b0
reema.joshi <reema.joshi@newgen.co.in>
2019-06-03 11:23:36 +0530

    created userdetails service bean.

--- src/main/java/com/learning/springboot/service/UserDetailsServiceImpl.java
+++ src/main/java/com/learning/springboot/service/UserDetailsServiceImpl.java
@@ -3,11 +3,13 @@ package com.learning.springboot.service;
 import com.learning.springboot.dao.UserDao;
 import com.learning.springboot.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -27,6 +29,41 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             arrayList.add(userName);
         }
         System.out.println("username list "+arrayList);
-        return null;
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return null;
+            }
+
+            @Override
+            public String getUsername() {
+                return null;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return false;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+        };
     }
 }
